f2c1745f98134a9b5594c79439f05d1a3f2b67c1
Ivan Sinyavskiy <stargot@gmail.com>
2019-03-07 14:45:03 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,3 +1,11 @@
+# vBulletin Credentials
 START_URL = 'https://example.com/admincp'
 AUTH_LOGIN = 'admin'
 AUTH_PASSWORD = 'admin'
+
+# SMTP Credentials
+SMTP_HOST = 'mail.example.com'
+SMTP_LOGIN = 'example@example.com'
+SMTP_PASSWORD = 'examplepassword'
+MAILER_DAEMON_EMAIL = 'Mailer-Daemon@host.example.com'
+
